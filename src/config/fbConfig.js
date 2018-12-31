1b6d2cf4920f8a3d3a4cd058607f84859c07df2a
shrn82256 <shrn82256@gmail.com>
2018-12-31 10:41:14 +0530

    initial commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,17 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var config = require('./fbConfigDetails.json');
-// Initialize Firebase
-/*
-var config = {
-  apiKey: "AIzaSyB4iR053dFKite06trS0vYG8EZN-ED96gs",
-  authDomain: "marioplan-bad21.firebaseapp.com",
-  databaseURL: "https://marioplan-bad21.firebaseio.com",
-  projectId: "marioplan-bad21",
-  storageBucket: "marioplan-bad21.appspot.com",
-  messagingSenderId: "138454192728"
-};
-*/
+
 firebase.initializeApp(config);
 firebase.firestore().settings({
   timestampsInSnapshots: true,
